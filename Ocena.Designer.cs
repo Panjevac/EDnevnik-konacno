5088325e093041b457fc41e02f1e4c5c28b5a5f6
Panjevac <92999190+Panjevac@users.noreply.github.com>
2022-04-30 19:31:12 +0200

    ЕДневник
    Коначна верзија

---
+++ Ocena.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace EDnevnik
+{
+    partial class Ocena
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmb_prof = new System.Windows.Forms.ComboBox();
+            this.cmb_ucenik = new System.Windows.Forms.ComboBox();
+            this.cmb_godina = new System.Windows.Forms.ComboBox();
+            this.cmb_ocena = new System.Windows.Forms.ComboBox();
+            this.cmb_predmet = new System.Windows.Forms.ComboBox();
+            this.cmb_odeljenje = new System.Windows.Forms.ComboBox();
+            this.date_picker = new System.Windows.Forms.DateTimePicker();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.data_grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.a1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_test = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmb_prof
+            // 
+            this.cmb_prof.FormattingEnabled = true;
+            this.cmb_prof.Location = new System.Drawing.Point(50, 53);
+            this.cmb_prof.Name = "cmb_prof";
+            this.cmb_prof.Size = new System.Drawing.Size(121, 24);
+            this.cmb_prof.TabIndex = 0;
+            this.cmb_prof.SelectedValueChanged += new System.EventHandler(this.cmb_prof_SelectedValueChanged);
+            // 
+            // cmb_ucenik
+            // 
+            this.cmb_ucenik.FormattingEnabled = true;
+            this.cmb_ucenik.Location = new System.Drawing.Point(251, 135);
+            this.cmb_ucenik.Name = "cmb_ucenik";
+            this.cmb_ucenik.Size = new System.Drawing.Size(121, 24);
+            this.cmb_ucenik.TabIndex = 1;
+            // 
+            // cmb_godina
+            // 
+            this.cmb_godina.FormattingEnabled = true;
+            this.cmb_godina.Location = new System.Drawing.Point(50, 135);
+            this.cmb_godina.Name = "cmb_godina";
+            this.cmb_godina.Size = new System.Drawing.Size(121, 24);
+            this.cmb_godina.TabIndex = 2;
+            this.cmb_godina.SelectedIndexChanged += new System.EventHandler(this.cmb_godina_SelectedValueChanged);
+            // 
+            // cmb_ocena
+            // 
+            this.cmb_ocena.FormattingEnabled = true;
+            this.cmb_ocena.Location = new System.Drawing.Point(457, 135);
+            this.cmb_ocena.Name = "cmb_ocena";
+            this.cmb_ocena.Size = new System.Drawing.Size(146, 24);
+            this.cmb_ocena.TabIndex = 3;
+            // 
+            // cmb_predmet
+            // 
+            this.cmb_predmet.FormattingEnabled = true;
+            this.cmb_predmet.Location = new System.Drawing.Point(251, 53);
+            this.cmb_predmet.Name = "cmb_predmet";
+            this.cmb_predmet.Size = new System.Drawing.Size(121, 24);
+            this.cmb_predmet.TabIndex = 4;
+            this.cmb_predmet.SelectedValueChanged += new System.EventHandler(this.cmb_predmet_SelectedValueChanged);
+            // 
+            // cmb_odeljenje
+            // 
+            this.cmb_odeljenje.FormattingEnabled = true;
+            this.cmb_odeljenje.Location = new System.Drawing.Point(457, 53);
+            this.cmb_odeljenje.Name = "cmb_odeljenje";
+            this.cmb_odeljenje.Size = new System.Drawing.Size(146, 24);
+            this.cmb_odeljenje.TabIndex = 5;
+            this.cmb_odeljenje.SelectedValueChanged += new System.EventHandler(this.cmb_odeljenje_SelectedValueChanged);
+            // 
+            // date_picker
+            // 
+            this.date_picker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.date_picker.Location = new System.Drawing.Point(50, 221);
+            this.date_picker.Name = "date_picker";
+            this.date_picker.Size = new System.Drawing.Size(200, 22);
+            this.date_picker.TabIndex = 6;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(457, 223);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(146, 22);
+            this.txt_id.TabIndex = 7;
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(197, 331);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_edit.TabIndex = 8;
+            this.btn_edit.Text = "Izmeni";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(330, 331);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 9;
+            this.btn_add.Text = "Dodaj";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(465, 331);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 10;
+            this.btn_delete.Text = "Obrisi";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // data_grid
+            // 
+            this.data_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_grid.Location = new System.Drawing.Point(664, 53);
+            this.data_grid.Name = "data_grid";
+            this.data_grid.RowHeadersWidth = 51;
+            this.data_grid.RowTemplate.Height = 24;
+            this.data_grid.Size = new System.Drawing.Size(442, 301);
+            this.data_grid.TabIndex = 11;
+            this.data_grid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_grid_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Profesor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Predmet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Odeljenje";
+            // 
+            // a1
+            // 
+            this.a1.AutoSize = true;
+            this.a1.Location = new System.Drawing.Point(47, 95);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(54, 17);
+            this.a1.TabIndex = 15;
+            this.a1.Text = "Godina";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(248, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Ucenik";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(454, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Ocena";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(454, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 17);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "ID";
+            // 
+            // txt_test
+            // 
+            this.txt_test.Location = new System.Drawing.Point(50, 383);
+            this.txt_test.Name = "txt_test";
+            this.txt_test.Size = new System.Drawing.Size(528, 22);
+            this.txt_test.TabIndex = 19;
+            // 
+            // Ocena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 450);
+            this.Controls.Add(this.txt_test);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.data_grid);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.date_picker);
+            this.Controls.Add(this.cmb_odeljenje);
+            this.Controls.Add(this.cmb_predmet);
+            this.Controls.Add(this.cmb_ocena);
+            this.Controls.Add(this.cmb_godina);
+            this.Controls.Add(this.cmb_ucenik);
+            this.Controls.Add(this.cmb_prof);
+            this.Name = "Ocena";
+            this.Text = "Ocena";
+            this.Load += new System.EventHandler(this.Ocena_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmb_prof;
+        private System.Windows.Forms.ComboBox cmb_ucenik;
+        private System.Windows.Forms.ComboBox cmb_godina;
+        private System.Windows.Forms.ComboBox cmb_ocena;
+        private System.Windows.Forms.ComboBox cmb_predmet;
+        private System.Windows.Forms.ComboBox cmb_odeljenje;
+        private System.Windows.Forms.DateTimePicker date_picker;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.DataGridView data_grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label a1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_test;
+    }
+}
---
+++ Ocena.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EDnevnik
+{
+    
+    public partial class Ocena : Form
+    {
+        DataTable grid;
+        public Ocena()
+        {
+            InitializeComponent();
+        }
+
+        private void cmb_godina_populate()
+        {
+            SqlConnection veza1 = Konekcija.Povezivanje();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from skolska_godina", veza1);
+            DataTable godina = new DataTable();
+            adapter.Fill(godina);
+
+            cmb_godina.DataSource = godina;
+            cmb_godina.DisplayMember = "naziv";
+            cmb_godina.ValueMember = "id";
+            cmb_godina.SelectedValue = 2;
+
+        }
+
+        private void cmb_profesor_populate()
+        {
+            SqlConnection veza1 = Konekcija.Povezivanje();
+            StringBuilder naredba = new StringBuilder("select osoba.id as id, ime+prezime as naziv from osoba ");
+            naredba.Append("join raspodela on osoba.id = nastavnik_id");
+            naredba.Append(" where raspodela.godina_id = " + cmb_godina.SelectedValue.ToString()) ;
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza1);
+            DataTable profesor = new DataTable();
+            adapter.Fill(profesor);
+
+            cmb_prof.Enabled = true;
+            cmb_prof.DataSource = profesor;
+            cmb_prof.DisplayMember = "naziv";
+            cmb_prof.ValueMember = "id";
+            cmb_prof.SelectedIndex = -1;
+
+
+        }
+        private void Ocena_Load(object sender, EventArgs e)
+        {
+            cmb_godina_populate();
+            cmb_predmet.Enabled = false;
+            cmb_prof.Enabled = false;
+            cmb_odeljenje.Enabled = false;
+            cmb_ucenik.Enabled = false;
+            cmb_ocena.Items.Add(1);
+            cmb_ocena.Items.Add(2);
+            cmb_ocena.Items.Add(3);
+            cmb_ocena.Items.Add(4);
+            cmb_ocena.Items.Add(5);
+        }
+
+        private void cmb_godina_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if(cmb_godina.IsHandleCreated && cmb_godina.Focused)
+            {
+                cmb_profesor_populate();
+                
+                //cmb_prof.SelectedIndex = -1;
+            }
+            
+        }
+        private void cmb_predmet_populate()
+        {
+            SqlConnection veza1 = Konekcija.Povezivanje();
+            StringBuilder naredba = new StringBuilder("select distinct predmet.id as id, naziv from predmet ");
+            naredba.Append(" join raspodela on predmet.id = predmet_id");
+            naredba.Append(" where godina_id = " + cmb_godina.SelectedValue.ToString());
+            naredba.Append(" and nastavnik_id= " + cmb_prof.SelectedValue.ToString());
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza1);
+            DataTable predmet = new DataTable();
+            adapter.Fill(predmet);
+
+            cmb_predmet.Enabled = true;
+            cmb_predmet.DataSource = predmet;
+            cmb_predmet.DisplayMember = "naziv";
+            cmb_predmet.ValueMember = "id";
+            cmb_predmet.SelectedIndex = -1;
+
+
+        }
+
+        private void cmb_prof_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmb_prof.IsHandleCreated && cmb_prof.Focused)
+            {
+                cmb_predmet_populate();
+                //cmb_prof.SelectedIndex = -1;
+            }
+        }
+
+        private void cmb_predmet_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmb_predmet.IsHandleCreated && cmb_predmet.Focused)
+            {
+                cmb_odeljenje_populate();
+                //cmb_prof.SelectedIndex = -1;
+            }
+        }
+
+        private void cmb_odeljenje_populate()
+        {
+            SqlConnection veza1 = Konekcija.Povezivanje();
+            StringBuilder naredba = new StringBuilder("select distinct odeljenje.id as id, str(razred)+'/'+str(indeks) as naziv from odeljenje ");
+            naredba.Append(" join raspodela on odeljenje.id = odeljenje_id");
+            naredba.Append(" where raspodela.godina_id = " + cmb_godina.SelectedValue.ToString());
+            naredba.Append(" and razredni_id= " + cmb_prof.SelectedValue.ToString());
+            naredba.Append(" and predmet_id= " + cmb_predmet.SelectedValue.ToString());
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza1);
+            DataTable odeljenje = new DataTable();
+            adapter.Fill(odeljenje);
+
+            cmb_odeljenje.Enabled = true;
+            cmb_odeljenje.DataSource = odeljenje;
+            cmb_odeljenje.DisplayMember = "naziv";
+            cmb_odeljenje.ValueMember = "id";
+            cmb_odeljenje.SelectedIndex = -1;
+
+
+        }
+
+        private void cmb_odeljenje_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmb_odeljenje.IsHandleCreated && cmb_odeljenje.Focused)
+            {
+                cmb_ucenik_populate();
+                grid_populate();
+                ucenik_ocena_id(0);
+                //cmb_prof.SelectedIndex = -1;
+            }
+        }
+        private void cmb_ucenik_populate()
+        {
+            SqlConnection veza1 = Konekcija.Povezivanje();
+            StringBuilder naredba = new StringBuilder("select distinct osoba.id as id, ime+prezime as naziv from osoba ");
+            naredba.Append(" join upisnica on osoba.id = osoba_id");
+            naredba.Append(" where upisnica.odeljenje_id = " + cmb_odeljenje.SelectedValue.ToString());
+     
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza1);
+            DataTable ucenik = new DataTable();
+            adapter.Fill(ucenik);
+
+            cmb_ucenik.Enabled = true;
+            cmb_ucenik.DataSource = ucenik;
+            cmb_ucenik.DisplayMember = "naziv";
+            cmb_ucenik.ValueMember = "id";
+            cmb_ucenik.SelectedIndex = -1;
+
+
+        }
+        private void grid_populate()
+        {
+            SqlConnection veza1 = Konekcija.Povezivanje();
+            StringBuilder naredba = new StringBuilder("select ocena.id as id, ime+prezime as naziv,ocena, ucenik_id, datum from osoba ");
+            naredba.Append(" join ocena on osoba.id = ucenik_id");
+            naredba.Append(" join raspodela on raspodela_id = raspodela.id");
+            naredba.Append(" where raspodela_id = ");
+            naredba.Append("(select id from raspodela where godina_id= "+ cmb_godina.SelectedValue.ToString());
+            naredba.Append(" and nastavnik_id= " + cmb_prof.SelectedValue.ToString());
+            naredba.Append(" and predmet_id= " + cmb_predmet.SelectedValue.ToString());
+            naredba.Append(" and odeljenje_id= " + cmb_odeljenje.SelectedValue.ToString()+")");
+            //txt_test.Text = naredba.ToString();
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza1);
+            grid = new DataTable();
+            adapter.Fill(grid);
+
+            data_grid.DataSource = grid;
+            data_grid.AllowUserToAddRows = false;
+
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            StringBuilder naredba = new StringBuilder("select id from raspodela");
+            naredba.Append(" where godina_id= " + cmb_godina.SelectedValue.ToString());
+            naredba.Append(" and nastavnik_id= " + cmb_prof.SelectedValue.ToString());
+            naredba.Append(" and predmet_id= " + cmb_predmet.SelectedValue.ToString());
+            naredba.Append(" and odeljenje_id= " + cmb_odeljenje.SelectedValue.ToString());
+            SqlConnection veza1 = Konekcija.Povezivanje();
+            SqlCommand komanda = new SqlCommand(naredba.ToString(), veza1);
+            int id_raspodele = 0;
+            try
+            {
+                veza1.Open();
+                id_raspodele=(int)komanda.ExecuteScalar();
+                veza1.Close();
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message);
+                throw;
+            }
+            if(id_raspodele>0)
+            {
+                naredba = new StringBuilder("insert into ocena(datum, raspodela_id, ucenik_id, ocena) values('");
+                DateTime datum = date_picker.Value;
+                naredba.Append(datum.ToString("yyyy-MM-dd")+"','");
+                naredba.Append(id_raspodele.ToString() + "','");
+                naredba.Append(cmb_ucenik.SelectedValue.ToString() + "','");
+                naredba.Append(cmb_ocena.SelectedItem.ToString() + "')");
+
+                komanda = new SqlCommand(naredba.ToString(), veza1);
+                try
+                {
+                    veza1.Open();
+                    id_raspodele = (int)komanda.ExecuteNonQuery();
+                    veza1.Close();
+                }
+                catch (Exception greska)
+                {
+                    MessageBox.Show(greska.Message);
+                    throw;
+                }
+                grid_populate();
+            }
+        }
+
+        
+        private void btn_edit_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(txt_id.Text) > 0)
+            {
+                DateTime datum = date_picker.Value;
+                StringBuilder naredba = new StringBuilder("update ocena set");
+                naredba.Append(" ucenik_id = '" + cmb_ucenik.SelectedValue.ToString()+"', ");
+                naredba.Append(" ocena = '" + cmb_ocena.SelectedItem.ToString() + "', ");
+                naredba.Append(" datum = '" + datum.ToString("yyyy-MM-dd") + "' ");
+                naredba.Append(" where id = " + txt_id.Text);
+                SqlConnection veza1 = Konekcija.Povezivanje();
+                SqlCommand komanda = new SqlCommand(naredba.ToString(), veza1);
+                
+                try
+                {
+                    veza1.Open();
+                    komanda.ExecuteNonQuery();
+                    veza1.Close();
+                }
+                catch (Exception greska)
+                {
+                    MessageBox.Show(greska.Message);
+                    throw;
+                }
+                
+                    grid_populate();
+                }
+            }
+
+        private void data_grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex>=0)
+            {
+                ucenik_ocena_id(e.RowIndex);
+            }
+        }
+
+        private void ucenik_ocena_id(int br_sloga)
+        {
+            cmb_ucenik.SelectedValue = grid.Rows[br_sloga]["ucenik_id"];
+            cmb_ocena.SelectedItem = grid.Rows[br_sloga]["ocena"];
+            txt_id.Text = grid.Rows[br_sloga]["id"].ToString();
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(txt_id.Text) > 0)
+            {
+                DateTime datum = date_picker.Value;
+                StringBuilder naredba = new StringBuilder("delete from ocena where id= "+ txt_id.Text);
+                
+                SqlConnection veza1 = Konekcija.Povezivanje();
+                SqlCommand komanda = new SqlCommand(naredba.ToString(), veza1);
+
+                try
+                {
+                    veza1.Open();
+                    komanda.ExecuteNonQuery();
+                    veza1.Close();
+                }
+                catch (Exception greska)
+                {
+                    MessageBox.Show(greska.Message);
+                    throw;
+                }
+
+                grid_populate();
+            }
+        }
+    }
+    }
+
